2b2207fda9fafee600ecf745994690455b06c0d3
Eric Stanulis <erstanulis@gmail.com>
2017-08-15 18:49:09 -0500

    Basic framework

---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+$(document).ready() {
+    var kvothe = {
+        hp : 120;
+        attackPower : 8;
+        counterattackPower : 20;
+    }
+    
+    var denna = {
+        hp : 120;
+        attackPower : 10;
+        counterattackPower : 5;
+    }
+
+    var haliax = {
+        hp : 180;
+        attackPower : 15;
+        counterattackPower : 25;
+    }
+
+    var cinder {
+        hp : 150;
+        attackPower : 12;
+        counterattackPower: 20;
+    }
+
+    document.on("click")
+
+}
