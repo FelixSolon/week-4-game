920cdb6846a9ad85b40ab58d955f92bb1ad47100
Eric Stanulis <erstanulis@gmail.com>
2017-08-17 16:53:29 -0500

    Can pick a character

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,62 @@
 $(document).ready(function() {
     var kvothe = {
-        hp : 120;
-        attackPower : 8;
-        counterattackPower : 20;
+        hp : 120
+        , attackPower : 8
+        , counterattackPower : 20
+        , picture : "assets/images/kvothe.jpg"
+        , name : "Kvothe"
     }
     
     var denna = {
-        hp : 120;
-        attackPower : 10;
-        counterattackPower : 5;
+        hp : 120
+        , attackPower : 10
+        , counterattackPower : 5
+        , picture : "assets/images/denna.jpg"
+        , name : "Denna"
     }
 
     var haliax = {
-        hp : 180;
-        attackPower : 15;
-        counterattackPower : 25;
+        hp : 180
+        , attackPower : 15
+        , counterattackPower : 25
+        , picture : "assets/images/haliax.jpeg"
+        , name : "Haliax"
     }
 
-    var cinder {
-        hp : 150;
-        attackPower : 12;
-        counterattackPower: 20;
+    var cinder = {
+        hp : 150
+        , attackPower : 12
+        , counterattackPower: 20
+        , picture : "assets/images/cinder.jpg"
+        , name : "Cinder"
     }
 
-    document.on("click")
-
-}
 
 
+    $("#kvothe").attr("src", kvothe.picture);
 
+    $("#denna").attr("src", denna.picture);
+
+    $("#haliax").attr("src", haliax.picture);
+
+    $("#cinder").attr("src", cinder.picture);
+
+    $("#kvotheText").text(kvothe.name);
+
+    $("#dennaText").text(denna.name);
+
+    $("#cinderText").text(cinder.name);
+
+    $("#haliaxText").text(haliax.name);
+
+
+    $("#cinderBlock").click(function(){
+        cinderHTML = $("#cinderBlock").html();
+        $("#chosenBlock").html(cinderHTML);
+        $(".imageBlock").addClass("invisible");
+        $("#chosenBlock").removeClass("invisible");
+    });
+
+
+//the end of the $(document).ready
 });
